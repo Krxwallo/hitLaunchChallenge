2cdf08748519ccb2744ab01430d1430814870a5f
Krxwallo <ken.grill@gmx.de>
2021-03-22 16:46:32 +0100

    Removed hello msg

--- src/me/justAm0dd3r/hitlaunchchallenge/events/EventListener.java
+++ src/me/justAm0dd3r/hitlaunchchallenge/events/EventListener.java
@@ -1,21 +1,13 @@
 package me.justAm0dd3r.hitlaunchchallenge.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.util.Vector;
 
 public class EventListener implements Listener {
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent evt) {
-        Bukkit.broadcastMessage("Player joined.");
-    }
-
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent evt) {
         if (evt.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK)
